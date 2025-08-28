5cbf0b9550da99c2561f8490237ced762fc219fb
dohee <doheeeq@gmail.com>
2025-08-28 15:37:43 +0900

    react: 종합 복습예제(타이머 챌린지) 점수 계산

---
+++ src/components/TimerGame/ResultModal.jsx
@@ -0,0 +1,29 @@
+import React, {forwardRef} from 'react';
+
+const ResultModal = forwardRef(({ result, targetTime, timeRemaining, onReset}, ref) => {
+
+    // 남은 시간을 밀리초에서 초로 변환
+    const formattedTime = (timeRemaining / 1000).toFixed(2);
+
+    // 점수 만들기
+    const score = Math.round((1 - timeRemaining / (targetTime * 1000)) * 100);
+
+
+    return (
+        <dialog className='result-modal' ref={ref}>
+            <h2>Your {result}!</h2>
+            {result === 'won' ? score : ''}
+            <p>
+                The target time was <strong>{targetTime} seconds.</strong>
+            </p>
+            <p>
+                You stopped the timer with <strong>{formattedTime} seconds left.</strong>
+            </p>
+            <form method='dialog'>
+                <button onClick={onReset}>Close</button>
+            </form>
+        </dialog>
+    )
+});
+
+export default ResultModal;
--- src/components/TimerGame/TimerChallenge.jsx
+++ src/components/TimerGame/TimerChallenge.jsx
@@ -1,5 +1,6 @@
 import React, {useRef, useState} from 'react';
-import ResultModal from "./ResultModdal.jsx";
+import ResultModal from './ResultModal.jsx';
+
 /*
   timer를 전역변수로 설정시 5초 start -> 10초 start -> 10초 stop -> 5초 stop
   을 순서대로 눌러보면 5초 타이머가 제대로 중지되지 않음
@@ -7,82 +8,86 @@ import ResultModal from "./ResultModdal.jsx";
   원인은 4개의 컴포넌트가 1개의 전역변수 timer를 공유함으로써 생기는 문제임
   처음 5초짜리의 타이머아이디가 10초짜리에게 덮어씌워져 사라짐.
 */
-// 타이어 id를 저장
+// 타이머 id를 저장
 // let timerId;
+
 
 const TimerChallenge = ({title, targetTime}) => {
 
-    // 타이머가 시작됐는지를 확인하는 상태값
-    const[timerStared, setTimerStarted] = useState(false);
-
-    // 타이머가 만료되었는지 확인하는 상태갑
-    const[timerExpired, setTimerExpired] = useState(false);
-
-    /*
-    timerId를 지역변수로 두자니 리렌더링시에 사라져버림
-    전역변수로 두자니 4개의컴포넌트가 공유해버리는 문제
-    상태변수로 두자니 상태변경이 복잡해진다.
-   */
     // 리렌더링이 되어도 값을 컴포넌트별로 유지하는 방법 useRef
     const timerId = useRef();
 
-    // 모달 태그를 제어하기 위한 ref
-    // Ref는 태그에 연결되어야 함.
+    // 모달태그를 제어하기 위한 ref
     const dialogRef = useRef();
 
+    // 남은 시간을 관리하는 상태변수
+    const [timeRemaining, setTimeRemaining] = useState(targetTime * 1000);
 
-    // 리렌더링이 되어도
+    // start stop 활성화 조건
+    const timerIsActive = timeRemaining > 0 && timeRemaining < targetTime * 1000;
 
-    // 타이머를 시작하는 이벤트
-    const handleStart = e => {
-        console.log('타이머가 시작됨!');
-        setTimerStarted(true);
-
-        // 실제 정해진 시간만큼 타이머를 가동
-        timerId.current = setTimeout(() => {
-            console.log(targetTime + 's 타이머 만료!');
-            setTimerExpired(true);
-
-            // document.querySelector('dialog').showModal();
-            console.log(dialogRef);
-            dialogRef.current.showModal();
-
-        }, targetTime * 1000);
-
-    }
-
-    /*
-      setTimeout 실행시 타이머의 id(aaa)가 생성된다.
-      타이머 실행시 setTimerExpired가 실행됨.
-
-      상태변수는 실행시 컴포넌트를 리렌더링함.
-      리렌더링할 때 기존에 저장한 timer id(aaa)를 없앤다.
-
-      따라서 제대로 clear를 수행하지 못함
-    */
-
-
-    // 타이머를 중단하는 이벤트
-    const handleStop = e => {
-        console.log('타이머가 중지됨! timerId -', timerId.current );
-        clearTimeout(timerId.current);
-
+    // 게임 패배 조건
+    if (timeRemaining <= 0) {
+        clearInterval(timerId.current);
         dialogRef.current.showModal();
     }
 
+    /*
+      timerId를 지역변수로 두자니 리렌더링시에 사라져버림
+      전역변수로 두자니 4개의컴포넌트가 공유해버리는 문제
+      상태변수로 두자니 상태변경이 복잡해진다.
+     */
+
+
+
+    // 타이머를 시작하는 이벤트
+    const handleStart = e => {
+        timerId.current = setInterval(() => {
+            setTimeRemaining((prevTime) => prevTime - 10);
+        }, 10);
+
+        /*
+          setTimeout실행시 타이머의 id(aaa)가 생성된다.
+          타이머 실행시 setTimerStarted가 실행됨.
+
+          상태변수는 setter실행시 컴포넌트를 리렌더링함.
+          리렌더링할 때 기존에 저장한 timer id(aaa)를 없앤다.
+
+          따라서 제대로 clear를 수행하지 못함
+        */
+    };
+
+    // 타이머를 중단하는 함수
+    const handleStop = e => {
+        console.log('타이머가 중지됨! timerId - ', timerId.current);
+        clearInterval(timerId.current);
+        dialogRef.current.showModal();
+    };
+
+    // 모달을 닫으면 남은시간을 초기상태로 리셋
+    const handleReset = () => setTimeRemaining(targetTime * 1000);
+
     return (
         <>
-            <ResultModal ref={dialogRef} result={'Lost'} targetTime={targetTime}/>
-            <section className='challenge'>
+            <ResultModal
+                ref={dialogRef}
+                result={timeRemaining <= 0 ? 'lost' : 'won'}
+                targetTime={targetTime}
+                timeRemaining={timeRemaining}
+                onReset={handleReset}
+            />
+            <section className="challenge">
                 <h2>{title}</h2>
-                <p className='challenge-time'>
+                <p className="challenge-time">
                     {targetTime} second{targetTime > 1 ? 's' : ''}
                 </p>
                 <p>
-                    <button onClick={timerStared ? handleStop : handleStart}>{timerStared ? 'stop' : 'start'} Challenge</button>
+                    <button onClick={timerIsActive ? handleStop : handleStart}>
+                        {timerIsActive ? 'Stop' : 'Start'} Challenge
+                    </button>
                 </p>
                 <p>
-                    {timerStared ? 'Time is running....' : 'Time inactive'}
+                    {timerIsActive ? 'Time is running...' : 'Timer inactive'}
                 </p>
             </section>
         </>
