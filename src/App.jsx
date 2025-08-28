89bd0139403dfc758f37f9db32684a81310fdede
dohee <doheeeq@gmail.com>
2025-08-28 10:16:36 +0900

    react: 에러 모달 띄우기

--- src/App.jsx
+++ src/App.jsx
@@ -1,50 +1,24 @@
 import React, {useState} from 'react';
 import './App.css';
-import TodoTemplate from './components/Todos/TodoTemplate.jsx';
+import AddUsers from './components/Users/AddUsers.jsx';
+import UserList from './components/Users/UserList.jsx';
 
 const App = () => {
 
-    const todoList = [
-        {
-            id: 't1',
-            todo: '할 일 어쩌구~~',
-            finish: false,
-        },
-        {
-            id: 't2',
-            todo: '할 일 어쩌구~~',
-            finish: false
-        }
-    ]
+    // 회원들이 저장될 배열
+    const [userList, setUserList] = useState([]);
 
-    const [todos, setTodos] = useState(todoList);
+    // 입력한 회원정보를 가져오는 함수
+    const addUserInfo = (user) => {
+        setUserList([...userList, user]);
+    };
 
-    const onAddTodo = (item) => {
-        // console.log(item)
-        setTodos(prev => [...prev, item])
-    }
-
-    const onCancelTodo = (item) => {
-        console.log(`삭제할 todo:`, item);
-        setTodos(prev => prev.filter(todo => todo.id !== item));
-    }
-
-    const onFinishTodo = (id) => {
-        // console.log(`토글된 todo: `, id);
-        // const findTodo = todoList.filter(todo => todo.id === id);
-        setTodos(prev => prev.map(todo => {
-            if (todo.id === id) {
-                todo.finish = !todo.finish;
-                return todo;
-            } else {
-                return todo;
-            }
-        }))
-
-    }
-
-
-    return <TodoTemplate onAdd={onAddTodo} todos = {todos} onCancel={onCancelTodo} length={todos.filter(todo => todo.finish !== true).length} onFinish={onFinishTodo}/>
+    return (
+        <>
+            <AddUsers onAddUser={addUserInfo} />
+            <UserList users={userList} />
+        </>
+    );
 };
 
 export default App;
---
+++ src/components/Users/AddUsers.jsx
@@ -0,0 +1,88 @@
+import React, {useState} from 'react';
+import styles from './AddUsers.module.css';
+import Card from '../ui/Card';
+import Button from '../ui/Button';
+import ErrorModal from "../ui/Modal/ErrorModal.jsx";
+
+const AddUsers = ({onAddUser}) => {
+    // 입력값들을 상태관리
+    const [userValue, setUserValue] = useState({
+        username: '',
+        age: '',
+    });
+
+    // 에러 데이터를 관리할 상태변수
+    // error -> {title: 에러제목, message: 에러원인}
+    const [error, setError] = useState(null)
+
+    const handleName = (e) => {
+        setUserValue({
+            ...userValue,
+            username: e.target.value,
+        });
+    };
+    const handleAge = (e) => {
+        setUserValue({
+            ...userValue,
+            age: e.target.value,
+        });
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        // 입력값 검증
+        if(!userValue.username.trim()) {
+            // 에러처리
+            setError({
+                title: '유효하지 않은 입력값',
+                message: '이름은 필수 입력값입니다.'
+            })
+            return;
+        }
+        if(+userValue.age < 1) {
+            // 에러처리
+            setError({
+                title: '유효하지 않은 나이 범위',
+                message: '나이는 0 또는 음수가 될 수 없습니다.'
+            })
+            return;
+        }
+
+        onAddUser({
+            ...userValue,
+            id: Math.random().toString(),
+        });
+
+        setUserValue({
+            username: '',
+            age: '',
+        });
+    };
+
+
+    return (
+        <>
+            { error && <ErrorModal title={error.title} message={error.message}/> }
+            <Card className={styles.input}>
+                    <form onSubmit={handleSubmit}>
+                        <label htmlFor='username'>이름</label>
+                        <input
+                            id='username'
+                            type='text'
+                            onInput={handleName}
+                        />
+                        <label htmlFor='age'>나이</label>
+                        <input
+                            id='age'
+                            type='number'
+                            onInput={handleAge}
+                        />
+                        <Button type='submit'>가입하기</Button>
+                    </form>
+                </Card>
+        </>
+    );
+};
+
+export default AddUsers;
